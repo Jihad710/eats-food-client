66dceea00e7b8b4fac8233115356dcc85c3f2224
unknown <jihadmahmud71@gmail.com>
2023-05-03 18:07:08 +0600

    login section shown

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -51,37 +51,7 @@ const Login = () => {
 
     return (
         <div>
-            <Container className='w-50 mx-auto'>
-            <h2 className='text-center'>Login Eats Food</h2>
-            <Form onSubmit={handleLogin}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control required name='email' type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control required name='password' type={showPassword? "text" : "password"} placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check  type="checkbox" label="Show password" checked={showPassword} onChange={toggleShowPassword}/>
-                </Form.Group>
-                <div className="d-grid gap-2">
-                <Button  variant="primary" type="submit">
-                    Login
-                </Button>
-    </div>
-                <p className=' my-3'><small>Don't have an Account? Please <Link to={`/register`} className='text-primary' >Sign up</Link></small></p>
-            </Form>
-            <hr className='w-75 mx-auto my-5'/>
-            <div className="d-grid gap-2">
-            <Button onClick={handleLoginWithGoogle} className='mb-2' variant="outline-primary"> <FaGoogle /> Login with Google</Button>
-            <Button onClick={handleLoginWithGitHub} variant="outline-secondary"> <FaGithub></FaGithub> Login with Github</Button>
-            </div>
-        </Container>
+           
         </div>
     );
 };
