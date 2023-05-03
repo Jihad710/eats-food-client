adb6d5dabab248a35593a574121d77f69c0331ec
unknown <jihadmahmud71@gmail.com>
2023-05-03 18:22:45 +0600

    registration section done

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -59,44 +59,7 @@ const Register = () => {
       };
     return (
         <div>
-             <Container className='w-50 mx-auto'>
-            <h2 className='text-center'>Sign up in to EatsExtra</h2>
-            <Form onSubmit={handleCreateUser}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control required name='name' type="text" placeholder="Enter name" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Photo URL</Form.Label>
-                    <Form.Control  name='photo' type="text" placeholder="Enter photo URL" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control required name='email' type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control autoComplete='off' required name='password' type='text' placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicRePassword">
-                    <Form.Label>Re-Password</Form.Label>
-                    <Form.Control autoComplete='off' required name='rePassword' type='text' placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check  type="checkbox" label={<span>Accept <Link to={`/terms`}> Terms & Condition</Link></span>} checked={check} onChange={handleAccept}/>
-                </Form.Group>
-                <div className="d-grid gap-2">
-                <Button className='fw-semibold'  disabled={!check} variant='warning' type='submit'>Sign Up</Button>
-    </div>
-                <p className=' my-3'><small>Already have an Account? Please <Link to={`/login`} className='text-primary' >Login</Link></small></p>
-            </Form>
-
-            
-        </Container>
+             
         </div>
     );
 };
